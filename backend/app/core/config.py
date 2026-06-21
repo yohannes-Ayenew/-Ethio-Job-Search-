80814ad753dfda5a48a66aaeecf8b114cf7a2134
yohannes-Ayenew <152222437+yohannes-Ayenew@users.noreply.github.com>
2026-06-21 23:57:49 +0300

    Update backend config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,15 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     MINI_APP_URL: str
+    ADMIN_ID: str
     DATABASE_URL: str
     UPSTASH_REDIS_REST_URL: str
     UPSTASH_REDIS_REST_TOKEN: str
     SECRET_KEY: str
     ENVIRONMENT: str = "development"
 
-    class Config:
-        env_file = (".env", "../.env")
+    model_config = SettingsConfigDict(
+        env_file=(".env", "../.env"),
+        extra="ignore"
+    )
 
 settings = Settings()
